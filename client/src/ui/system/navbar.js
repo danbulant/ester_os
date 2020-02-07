e4678b775be2d968e06d3c674e59d5da69db8b0e
danbulant <danbulant@gmail.com>
2020-02-07 20:08:48 +0100

    Testing navbar

---
+++ client/src/ui/system/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../App.css";
+
+function Navbar(props){
+
+    return (
+        <div className="navbar">
+            Navbar
+        </div>
+    );
+}
+
+export default Navbar;
