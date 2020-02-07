fd5d6291a6a796da6331ace95c7e4fd013ad7042
danbulant <danbulant@gmail.com>
2020-02-07 22:05:09 +0100

    Working change between time and password prompt

--- client/src/ui/system/login.js
+++ client/src/ui/system/login.js
@@ -1,12 +1,70 @@
 import React from 'react';
 import "../../App.css";
+//import MT from 'mousetrap';
 
-function Login(props){
-    return (
-        <div className="login">
-            Press ENTER to open login
-        </div>
-    )
+class Login extends React.Component{
+    constructor(p){
+        super(p)
+        this.state = {
+            login: false
+        };
+    }
+    componentDidMount(){
+        let loginPage = document.getElementById("loginPage");
+
+        let time = loginPage.getElementsByClassName("time")[0];
+        let user = loginPage.getElementsByClassName("user")[0];
+
+        let cl = this;
+        function showLogin(e) {
+            document.removeEventListener("keydown", showLogin);
+            console.log("Showing login");
+            cl.setState({login: true});
+
+            setTimeout(()=>{
+                console.log("Hiding login");
+                cl.setState({login: false});
+                document.addEventListener("keydown", showLogin);
+            }, 30000)
+        }
+
+        document.addEventListener("keydown", showLogin);
+    }
+    render(props){
+        if (this.state.login) {
+            return (
+                <div id="loginPage" className="login active">
+                    <div className="time active">
+                        00:00
+                    </div>
+                    <div className="user active">
+                        <div className="avatar-holder">
+                            <img className="avatar" alt="avatar" />
+                        </div>
+                        <div className="password-holder">
+                            <input type="password" className="password" />
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return (
+            <div id="loginPage" className="login">
+                <div className="time">
+                    20:48
+                    Press ENTER to open login
+                </div>
+                <div className="user">
+                    <div className="avatar-holder">
+                        <img className="avatar" alt="avatar" />
+                    </div>
+                    <div className="password-holder">
+                        <input type="password" className="password" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Login;
