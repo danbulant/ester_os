98c95c0299f8ddc38dd4bd15443f7f34754fa549
danbulant <danbulant@gmail.com>
2020-02-07 22:39:49 +0100

    Remove setBackground function

--- client/src/ui/main.js
+++ client/src/ui/main.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Navbar from './system/navbar';
-import setBackground from './system/utils/background';
 import Desktop from './system/desktop'
 import Login from './system/login';
 import '../App.css';
 
 function App() {
-    setBackground();
-
     if(global.user === undefined){
         return (
             <Login />
