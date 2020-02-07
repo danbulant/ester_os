2025caad8cb88fc2ba3d018cff4ae860d865b136
danbulant <danbulant@gmail.com>
2020-02-07 20:40:40 +0100

    Sample desktop

---
+++ client/src/ui/system/desktop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../App.css";
+
+function Desktop(props){
+    return (
+        <div className="desktop">
+            Desktop icons will be here
+        </div>
+    );
+}
+
+export default Desktop;
