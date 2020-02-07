814f67617d0a4035b5b559c1204bf68c0e7b2f69
danbulant <danbulant@gmail.com>
2020-02-07 20:40:08 +0100

    Start working on login screen

--- client/src/ui/main.js
+++ client/src/ui/main.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Navbar from './system/navbar';
-import setBackground from './system/background';
+import setBackground from './system/utils/background';
+import Desktop from './system/desktop'
+import Login from './system/login';
 import '../App.css';
 
 function App() {
     setBackground();
-    
-    return (
-        <>
-            <Navbar />
-        </>
-    );
+
+    if(global.user === undefined){
+        return (
+            <Login />
+        )
+    } else {
+        return (
+            <>
+                <Desktop />
+                <Navbar />
+            </>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/ui/system/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../App.css";
+
+function Login(props){
+    return (
+        <div className="login">
+            Press ENTER to open login
+        </div>
+    )
+}
+
+export default Login;
