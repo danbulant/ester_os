fa1868bfe4c24eefa22a90c8582d3ce8d4cbd595
danbulant <danbulant@gmail.com>
2020-02-07 20:41:21 +0100

    Dummy user verifier

---
+++ client/src/ui/system/utils/verifyUser.js
@@ -0,0 +1,10 @@
+
+//To-Do: verify user with API
+function verifyUser(name, password){
+    return new Promise((resolve, reject) => {
+        if(password = "1234")resolve(true);
+        resolve(false);
+    })
+}
+
+export default verifyUser;
