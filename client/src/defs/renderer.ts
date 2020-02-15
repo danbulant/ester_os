a81cc3a631cdb670897c489e1918d21ca1b61564
danbulant <danbulant@gmail.com>
2020-02-15 18:04:26 +0100

    Renderer interface

---
+++ client/src/defs/renderer.ts
@@ -0,0 +1,18 @@
+export default interface renderer {
+    name: string;
+    removePrevious: boolean;
+
+    /**
+     * Possible states:
+     * 0    not-ready (cannot be rendered)
+     * 1    ready (can be rendered)
+     * 2    rendered (user can see this)
+     * 3    hidden (can be rendered again)
+     * 4    ejected (cannot be rendered again)
+     */
+    state: 0 | 1 | 2 | 3 | 4;
+
+    render(): void;
+    eject(): void;
+    hide?(): void;
+}
