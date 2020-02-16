68ddaf90a891bd75ca21e5d6984ebb5c7bccee17
danbulant <danbulant@gmail.com>
2020-02-16 17:49:44 +0100

    fix regex & throw invalid username when username won't

--- client/src/managers/login.ts
+++ client/src/managers/login.ts
@@ -1,11 +1,11 @@
 import User from './user';
-import NotYetImplemented from '../errors/notYetImplemented';
+import {NotYetImplemented, InvalidUsername} from '../errors/index';
 
 class Login {
     user: User = null;
 
     checkName(name: string): boolean{
-        return /[a-z0-9_-](?:@[a-z0-9_-])/gi.test(name);
+        return /[a-z0-9_-]{1,}(@[a-z0-9_-]{1,})?/gi.test(name);
     }
 
     async fetchAvatar(name: string): Promise<string>{
@@ -13,6 +13,7 @@ class Login {
     }
 
     async login(name: string, password: string): Promise<User> {
+        if(!this.checkName(name))throw new InvalidUsername();
         throw new NotYetImplemented();
     }
 }
